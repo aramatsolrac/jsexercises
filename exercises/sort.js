b5caa3813486520da179854d7337a5e109284ed2
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-17 18:42:20 -0600

    Add solution

--- exercises/sort.js
+++ exercises/sort.js
@@ -1,6 +1,15 @@
 function sort(array) {
-    // TODO: sort the array and return the result
-    return array;
+  for (let i = 1; i < array.length; i++) {
+    let current = array[i]; // current = item at index 1  -> 10
+    let j = i - 1; // j = index 0
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j]; // put to right
+      j--;
+    }
+    array[j + 1] = current; // put to left
+  }
+  console.log(array);
+  return array;
 }
 
 module.exports = sort;
