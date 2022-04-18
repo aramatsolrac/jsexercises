bb01e92736611355fb7eb94815048793d41ce56f
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-17 18:50:23 -0600

    Fix comments

--- exercises/sort.js
+++ exercises/sort.js
@@ -1,7 +1,7 @@
 function sort(array) {
   for (let i = 1; i < array.length; i++) {
-    let current = array[i]; // current = item at index 1  -> 10
-    let j = i - 1; // j = index 0
+    let current = array[i]; // current = item at index i
+    let j = i - 1; // j = index of item to the left of current
     while (j >= 0 && array[j] > current) {
       array[j + 1] = array[j]; // put to right
       j--;
